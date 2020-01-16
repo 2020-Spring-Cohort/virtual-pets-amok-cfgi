0e912430bec3a5aeb1072249e705c2353a93c5ee
Colin Wakefield <colinpwakefield@gmail.com>
2020-01-16 15:10:27 -0500

    Test and App classes

--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -1,4 +1,12 @@
 package virtual_pet;
 
+import org.junit.jupiter.api.Test;
+
 public class VirtualPetTest {
+
+    @Test
+    public void shouldInstantiate() {
+        VirtualPet pet = new VirtualPet();
+    }
+
 }
