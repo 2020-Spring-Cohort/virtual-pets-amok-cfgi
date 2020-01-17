c65df291ffda8745422e7437e320ca6650c2f795
Colin Wakefield <colinpwakefield@gmail.com>
2020-01-17 10:32:47 -0500

    Updated test suite

--- src/main/java/virtual_pet/VirtualPet.java
+++ src/main/java/virtual_pet/VirtualPet.java
@@ -5,20 +5,36 @@ public class VirtualPet {
     private final String name;
     private int age;
     private final String color;
-    private int weight;
-    private String size;
     private int hunger;
     private int happiness;
     private String feelings;
 
-    public VirtualPet(String name, int age, String color, int weight, String size, int hunger, int happiness, String feelings) {
+    // Parameterized constructor
+    public VirtualPet(String name, int age, String color, int hunger, int happiness, String feelings) {
         this.name = name;
         this.age = age;
         this.color = color;
-        this.weight = weight;
-        this.size = size;
         this.hunger = hunger;
         this.happiness = happiness;
         this.feelings = feelings;
+    }
+
+    // Default constructor
+    public VirtualPet() {
+
+        this.name = "John";
+
+        this.color = "Brown";
+
+        this.hunger = 10;
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getHunger() {
+        return hunger;
     }
 }
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -2,6 +2,8 @@ package virtual_pet;
 
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class VirtualPetTest {
 
     @Test
@@ -9,4 +11,21 @@ public class VirtualPetTest {
         VirtualPet pet = new VirtualPet();
     }
 
+    @Test
+    public void shouldSayTheirName() {
+        VirtualPet pet = new VirtualPet();
+
+        String result = pet.getName();
+
+        assertEquals("John", result);
+    }
+
+    @Test
+    public void shouldHaveTenHunger() {
+        VirtualPet pet = new VirtualPet();
+
+        int result = pet.getHunger();
+
+        assertEquals(10, result);
+    }
 }
