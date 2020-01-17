2ab128ca4df81075bb52ca74c5e8a2b4a0369e4c
Colin Wakefield <colinpwakefield@gmail.com>
2020-01-17 14:04:48 -0500

    Added List field

--- src/main/java/virtual_pet/VirtualPetShelter.java
+++ src/main/java/virtual_pet/VirtualPetShelter.java
@@ -1,6 +1,13 @@
 package virtual_pet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class VirtualPetShelter {
-    public void getPet() {
+
+    private List<VirtualPet> pets = new ArrayList<>();
+
+    public List<VirtualPet> getPets() {
+        return pets;
     }
 }
