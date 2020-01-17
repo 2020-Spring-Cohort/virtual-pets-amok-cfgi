3629aa48b9a58945a1570c4bc14b78c813f67cba
Gaelan Shively <gaelanshively@gmail.com>
2020-01-17 14:17:37 -0500

    added user input options

--- src/main/java/virtual_pet/Application.java
+++ src/main/java/virtual_pet/Application.java
@@ -1,7 +1,5 @@
 package virtual_pet;
 
-import org.w3c.dom.ls.LSOutput;
-
 import java.util.Scanner;
 
 public class Application {
@@ -14,11 +12,29 @@ public class Application {
         VirtualPet firstPet = new VirtualPet(name);
         System.out.println(firstPet.getName());
 
-
+        for (; ; ) {
+            System.out.println("What would you like to do?");
+            String userCommand = scanner.nextLine();
+            switch (userCommand) {
+                case "feed":
+                    firstPet.feed();
+                    break;
+                case "play":
+                    firstPet.play();
+                    break;
+                default:
+                    System.out.println("Unknown command, please try again.");
+            }
+            firstPet.tick();
+            showPetInfo(firstPet);
+        }
     }
 
-
-
+    public static void showPetInfo(VirtualPet pet) {
+        System.out.println(pet.getName());
+        System.out.println("Hunger: " + pet.getHunger());
+        System.out.println("Happiness: " + pet.getHappiness());
+    }
 
 
 }
--- src/test/java/virtual_pet/VirtualPetTest.java
+++ src/test/java/virtual_pet/VirtualPetTest.java
@@ -141,4 +141,5 @@ public class VirtualPetTest {
         assertEquals(13, pet.getHunger());
         assertEquals(47, pet.getHappiness());
     }
+
 }
