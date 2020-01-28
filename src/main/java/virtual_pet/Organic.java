c5c0319b21aec14ba84c6c7444f443d44e2d040a
Fatuma Pemba <fatuma.pemba@yahoo.com>
2020-01-28 10:36:19 -0500

    deleted foodtype

--- src/main/java/virtual_pet/Organic.java
+++ src/main/java/virtual_pet/Organic.java
@@ -2,43 +2,21 @@ package virtual_pet;
 
 public abstract class Organic extends VirtualPet {
 
-    private int exercise;
+
     private boolean asleep;
-    private String foodType;
     private boolean needsToPoop;
 
-
-    public int getExercise() {
-        return exercise;
-    }
 
     public boolean isAsleep() {
         return asleep;
     }
 
-    public String getFoodType() {
-        return foodType;
-    }
 
     public boolean isNeedsToPoop() {
         return needsToPoop;
     }
 
-    public void setExercise(int exercise) {
-        this.exercise = exercise;
-    }
 
-    public void setFoodType(String foodType) {
-        this.foodType = foodType;
-    }
 
-//    public void clean(){
-//        if(exercise > 50){
-//            System.out.println("Cage is clean");
-//        } else {
-//            System.out.println("Cage  is dirty.");
-//        }
-//
-//    }
 
 }
