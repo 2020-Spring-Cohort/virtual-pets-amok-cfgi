5d574936d6ea2ef44348f65b3cbba1e259dce639
Fatuma Pemba <fatuma.pemba@yahoo.com>
2020-01-28 10:54:26 -0500

    removed sleeping

--- src/main/java/virtual_pet/Organic.java
+++ src/main/java/virtual_pet/Organic.java
@@ -2,15 +2,7 @@ package virtual_pet;
 
 public abstract class Organic extends VirtualPet {
 
-
-    private boolean asleep;
     private boolean needsToPoop;
-
-
-    public boolean isAsleep() {
-        return asleep;
-    }
-
 
     public boolean isNeedsToPoop() {
         return needsToPoop;
